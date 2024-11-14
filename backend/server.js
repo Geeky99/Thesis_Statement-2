f16e71f66a66d729fa2956c4e4ac0439dbd11fbb
Aaliyah Haynes <aahaynes@claflin.edu>
2024-11-14 12:38:39 -0500

    New Changes

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const app = express()
+const mysql = require('mysql')
+const db = mysql.createConnection({
+    
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "registration"
+})
