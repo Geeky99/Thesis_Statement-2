dcee84ab0d72db98abfc2352a854f56121287f62
Aaliyah Haynes <aahaynes@claflin.edu>
2024-11-14 13:38:08 -0500

    New Changes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,23 @@
-const app = express()
-const mysql = require('mysql')
-const db = mysql.createConnection({
-    
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "registration"
-})
+const mysql2 = require("mysql2");
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcryptjs = require("bcryptjs");
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+// MySQL Connection
+const con = mysql2.createConnection({
+
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "registration",
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
